2f202363feba8c8da031b3c87add2143e4d5d0b7
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-22 05:44:41 +0100

    Add verbosity

--- split_feature_scenarios_and_run_in_parallel.sh
+++ split_feature_scenarios_and_run_in_parallel.sh
@@ -2,6 +2,8 @@ function split_feature_scenarios_and_run_in_parallel() {
   local split_feature_scenarios_and_run_in_parallel_FILE
   for split_feature_scenarios_and_run_in_parallel_FILE in $@
   do
-    DISABLE_SPRING=1 echorun bundle exec rake "parallel:features[,'`split_feature_scenarios $split_feature_scenarios_and_run_in_parallel_FILE`']" || return $?
+    local split_feature_scenarios_and_run_in_parallel_DIR=`split_feature_scenarios $split_feature_scenarios_and_run_in_parallel_FILE`
+    find $split_feature_scenarios_and_run_in_parallel_DIR -type f -name "*.feature"
+    DISABLE_SPRING=1 echorun bundle exec rake "parallel:features[,'$split_feature_scenarios_and_run_in_parallel_DIR']" || return $?
   done
 }
