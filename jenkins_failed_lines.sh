2ba8f6d87582d59fdce1ede9b6398a98fa595d06
Marcin Nowicki <pr0d1r2@gmail.com>
2017-01-06 08:54:01 +0100

    Clone project when missing

--- jenkins_failed_lines.sh
+++ jenkins_failed_lines.sh
@@ -5,6 +5,10 @@ function jenkins_failed_lines() {
     git clone git@github.com:pr0d1r2/jenkins_failed_lines.git ~/projects/jenkins_failed_lines || return $?
     cd ~/projects/jenkins_failed_lines || return $?
     bi || return $?
+  else
+    cd ~/projects/jenkins_failed_lines || return $?
+    git pull &>/dev/null
+    bi &>/dev/null
   fi
   cd ~/projects/jenkins_failed_lines || return $?
   be ruby jenkins_failed_lines.rb $@
