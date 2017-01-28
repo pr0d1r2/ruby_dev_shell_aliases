a750132ef2c3f33a0dd786f85e5711a1628a1752
Marcin Nowicki <pr0d1r2@gmail.com>
2017-01-28 06:24:26 +0100

    Get count from size of array

--- run_spec_for.sh
+++ run_spec_for.sh
@@ -5,13 +5,11 @@
 # run_spec_for app/model/user.rb # will run `rspec spec/model/user_spec.rb`
 # run_spec_for spec/model/user_spec.rb:8472 # will run `rspec spec/model/user_spec.rb:8472`
 function run_spec_for() {
-  local run_spec_for_COUNT=0
   local run_spec_for_FILE
   local run_spec_for_FILES=()
   for run_spec_for_FILE in `existing_spec_for $@`
   do
     run_spec_for_FILES+=$run_spec_for_FILE
-    run_spec_for_COUNT+=1
   done
   local run_spec_for_SINGLE_LINES=()
   for run_spec_for_FILE in $@
@@ -25,9 +23,9 @@ function run_spec_for() {
   if [ ${#run_spec_for_SINGLE_LINES[@]} -gt 0 ]; then
     echorun bers $run_spec_for_SINGLE_LINES || return $?
   fi
-  if [ $run_spec_for_COUNT -eq 1 ]; then
+  if [ ${#run_spec_for_FILES[@]} -eq 1 ]; then
     echorun bers $run_spec_for_FILES || return $?
-  elif [ $run_spec_for_COUNT -gt 1 ]; then
+  elif [ ${#run_spec_for_FILES[@]} -gt 1 ]; then
     echorun beps $run_spec_for_FILES || return $?
   else
     echo "No specs for $@ !!!"
