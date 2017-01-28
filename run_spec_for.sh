500e8a4adb3a5f9e5f3bc0fa1f24893e74ba7ad7
Marcin Nowicki <pr0d1r2@gmail.com>
2017-01-28 06:20:53 +0100

    Add support for single lines

--- run_spec_for.sh
+++ run_spec_for.sh
@@ -7,6 +7,18 @@ function run_spec_for() {
     run_spec_for_FILES+=$run_spec_for_FILE
     run_spec_for_COUNT+=1
   done
+  local run_spec_for_SINGLE_LINES=()
+  for run_spec_for_FILE in $@
+  do
+    case $run_spec_for_FILE in
+      *_spec.rb:[1-9] | *_spec.rb:[1-9][0-9] | *_spec.rb:[1-9][0-9][0-9] | *_spec.rb:[1-9][0-9][0-9][0-9] | *_spec.rb:[1-9][0-9][0-9][0-9][0-9])
+        run_spec_for_SINGLE_LINES+=$run_spec_for_FILE
+        ;;
+    esac
+  done
+  if [ ${#run_spec_for_SINGLE_LINES[@]} -gt 0 ]; then
+    echorun bers $run_spec_for_SINGLE_LINES || return $?
+  fi
   if [ $run_spec_for_COUNT -eq 1 ]; then
     echorun bers $run_spec_for_FILES || return $?
   elif [ $run_spec_for_COUNT -gt 1 ]; then
