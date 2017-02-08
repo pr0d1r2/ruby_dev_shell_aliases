79907c15297ff54abb6872d929080129328ae863
Marcin Michałowski <marcin@stick.pl>
2017-02-08 16:19:02 +0100

    Use bundler version for the project to run bundle install

--- bi.sh
+++ bi.sh
@@ -1,9 +1,11 @@
 unalias bi
 function bi() {
+  local bi_BUNDLER_VERSION_PARAM=""
   if [ -e Gemfile.lock ]; then
     cat Gemfile.lock | grep -q 'BUNDLED WITH'
     if [ $? -eq 0 ]; then
       local bi_BUNDLED_WITH_VERSION=`echo $(tail -n1 Gemfile.lock)`
+      bi_BUNDLER_VERSION_PARAM="_${bi_BUNDLED_WITH_VERSION}_"
       case `bundler --version | cut -f 3 -d ' '` in
         $bi_BUNDLED_WITH_VERSION)
           ;;
@@ -15,8 +17,8 @@ function bi() {
   fi
   is_new_bundler
   if [ $? -eq 0 ]; then
-    echorun bundle install -j`bundler_threads` || return $?
+    echorun bundle $bi_BUNDLER_VERSION_PARAM install -j`bundler_threads` || return $?
   else
-    echorun bundle install || return $?
+    echorun bundle $bi_BUNDLER_VERSION_PARAM install || return $?
   fi
 }
