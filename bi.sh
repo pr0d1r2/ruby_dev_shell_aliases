312603ef667d4624ea9ba2ce262b688d5551104f
Marcin Michalowski <marcin@stick.pl>
2017-02-08 16:58:37 +0100

    Remove redundant line

--- bi.sh
+++ bi.sh
@@ -1,11 +1,10 @@
 unalias bi
 function bi() {
-  local bi_BUNDLER_VERSION_PARAM=""
   if [ -e Gemfile.lock ]; then
     cat Gemfile.lock | grep -q 'BUNDLED WITH'
     if [ $? -eq 0 ]; then
       local bi_BUNDLED_WITH_VERSION=`echo $(tail -n1 Gemfile.lock)`
-      bi_BUNDLER_VERSION_PARAM="_${bi_BUNDLED_WITH_VERSION}_"
+      local bi_BUNDLER_VERSION_PARAM="_${bi_BUNDLED_WITH_VERSION}_"
       case `bundler --version | cut -f 3 -d ' '` in
         $bi_BUNDLED_WITH_VERSION)
           ;;
