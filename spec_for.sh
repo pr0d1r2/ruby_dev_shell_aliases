ab35be5fd22593190ec62aa48fb5de2d3d5b9861
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-16 11:21:21 +0100

    Allow to pass directory as an argument

--- spec_for.sh
+++ spec_for.sh
@@ -1,16 +1,24 @@
 function spec_for() {
   local spec_for_FILE
   local spec_for_SPEC_FILE
+  local spec_for_FILE_IN_DIRECTORY
   for spec_for_FILE in $@
   do
-    spec_for_SPEC_FILE=`echo $spec_for_FILE | sed -e "s/app\//spec\//" | sed -e 's/.rb$/_spec.rb/' | sed -e 's/.slim$/.slim_spec.rb/'`
-    case $spec_for_SPEC_FILE in
-      spec/*)
-        ;;
-      *)
-        spec_for_SPEC_FILE="spec/$spec_for_SPEC_FILE"
-        ;;
-    esac
-    echo $spec_for_SPEC_FILE | grep --color=never "_spec.rb$" | sed -e 's/_spec_spec.rb$/_spec.rb/'
+    if [ -d $spec_for_FILE ]; then
+      for spec_for_FILE_IN_DIRECTORY in `find $spec_for_FILE -type f`
+      do
+        spec_for $spec_for_FILE_IN_DIRECTORY
+      done
+    else
+      spec_for_SPEC_FILE=`echo $spec_for_FILE | sed -e "s/app\//spec\//" | sed -e 's/.rb$/_spec.rb/' | sed -e 's/.slim$/.slim_spec.rb/'`
+      case $spec_for_SPEC_FILE in
+        spec/*)
+          ;;
+        *)
+          spec_for_SPEC_FILE="spec/$spec_for_SPEC_FILE"
+          ;;
+      esac
+      echo $spec_for_SPEC_FILE | grep --color=never "_spec.rb$" | sed -e 's/_spec_spec.rb$/_spec.rb/'
+    fi
   done
 }
