fc34d3bbd173a5b7fdba3c88a3f640a7157616d5
Marcin Nowicki <pr0d1r2@gmail.com>
2017-03-02 13:33:04 +0100

    Simplify syntax

--- bi_nokogiri_1_6_8_with_xz.sh
+++ bi_nokogiri_1_6_8_with_xz.sh
@@ -1,5 +1,5 @@
 function bi_nokogiri_1_6_8_with_xz() {
-  which xz 1>/dev/null 2>/dev/null
+  which xz &>/dev/null
   local bi_nokogiri_1_6_8_with_xz_NOT_PRESENT=$?
   if [ $bi_nokogiri_1_6_8_with_xz_NOT_PRESENT -eq 0 ]; then
     brew uninstall --ignore-dependencies xz || return $?
