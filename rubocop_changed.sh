abceb539f473ec1dad990d99302d247825856e83
Marcin Nowicki <pr0d1r2@gmail.com>
2017-03-04 16:39:47 +0100

    Perform parallel operation for RuboCop

--- rubocop_changed.sh
+++ rubocop_changed.sh
@@ -1,3 +1,3 @@
 function rubocop_changed() {
-  git status --porcelain | grep -v "^D" | cut -c4- | grep "\.rb$" | grep -v "^db/schema.rb$" | xargs `rubocop_executable`
+  git status --porcelain | grep -v "^D" | cut -c4- | grep "\.rb$" | grep -v "^db/schema.rb$" | parallel `rubocop_executable`
 }
