0d27375e361d88c00112fd00fd4fafc76d94ec9d
Marcin Nowicki <pr0d1r2@gmail.com>
2017-03-27 08:05:43 +0200

    Newer nokogiri still has problems with xz

---
+++ bi_nokogiri_with_xz.sh
@@ -0,0 +1,11 @@
+function bi_nokogiri_with_xz() {
+  which xz &>/dev/null
+  local bi_nokogiri_with_xz_NOT_PRESENT=$?
+  if [ $bi_nokogiri_with_xz_NOT_PRESENT -eq 0 ]; then
+    brew uninstall --ignore-dependencies --force xz || return $?
+  fi
+  bi || return $?
+  if [ $bi_nokogiri_with_xz_NOT_PRESENT -eq 0 ]; then
+    brew install xz || return $?
+  fi
+}
