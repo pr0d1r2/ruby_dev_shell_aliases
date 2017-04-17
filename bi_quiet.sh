a333eb78258b76ebb789afb9a333adb775c4a9ac
Marcin Nowicki <pr0d1r2@gmail.com>
2017-04-17 09:17:59 +0200

    Re-use silently command

--- bi_quiet.sh
+++ bi_quiet.sh
@@ -1,10 +1,3 @@
 function bi_quiet() {
-  local bi_quiet_STATUS
-  bi &> /tmp/.bi_quiet-$$.log
-  bi_quiet_STATUS=$?
-  if [ $bi_quiet_STATUS -gt 0 ]; then
-    cat /tmp/.bi_quiet-$$.log
-  fi
-  rm -f /tmp/.bi_quiet-$$.log
-  return $bi_quiet_STATUS
+  silently bi || return $?
 }
