594d8fd1a1ccd61ea511e40e2b24ecc6c210aec6
Marcin Nowicki <pr0d1r2@gmail.com>
2017-05-30 09:14:05 +0200

    Use proper reload for newer homebrew

--- pgreload_mac.sh
+++ pgreload_mac.sh
@@ -1,10 +1,3 @@
 function pgreload_mac() {
-  local pgreload_mac_PLIST="$HOME/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
-
-  launchctl list | grep -q homebrew.mxcl.postgresql
-  if [ $? -eq 0 ]; then
-    echorun launchctl unload $pgreload_mac_PLIST || return $?
-  fi
-
-  echorun launchctl load $pgreload_mac_PLIST || return $?
+  echorun brew services restart postgresql || return $?
 }
