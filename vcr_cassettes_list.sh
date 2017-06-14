f69f5b021e7fb20004147d80f19c701f037e9463
Marcin Nowicki <pr0d1r2@gmail.com>
2017-06-14 11:23:00 +0200

    Allow to specify on per-project where VCRs are stored

--- vcr_cassettes_list.sh
+++ vcr_cassettes_list.sh
@@ -1,5 +1,12 @@
 vcr_cassettes_list() {
-  find spec/fixtures/cassettes -name '*.yml' | \
-    parallel 'basename {}' | \
-    cut -f 1 -d .
+  if [ -f .vcr_cassettes_in ]; then
+    cat .vcr_cassettes_in | \
+      parallel "find {} -name '*.yml'" | \
+      parallel 'basename {}' | \
+      cut -f 1 -d .
+  else
+    find spec/fixtures/cassettes -name '*.yml' | \
+      parallel 'basename {}' | \
+      cut -f 1 -d .
+  fi
 }
