fc9178bc764a4a7cab0599938518b618ddc1d811
Marcin Nowicki <pr0d1r2@gmail.com>
2017-06-14 10:51:04 +0200

    Allow to specify on per-project where VCRs are used

--- not_existing_in_test_suite.sh
+++ not_existing_in_test_suite.sh
@@ -3,12 +3,19 @@ not_existing_in_test_suite() {
   not_existing_in_test_suite_COMMAND="rg --quiet $1 {};"
   not_existing_in_test_suite_COMMAND+='if [ $? -eq 0 ]; then exit 1; else exit 0; fi'
 
-  parallel \
-    --halt-on-error now,fail=1 \
-    "$not_existing_in_test_suite_COMMAND" \
-    ::: \
-    features \
-    spec
+  if [ -f .vcr_used_in ]; then
+    cat .vcr_used_in | \
+      parallel \
+        --halt-on-error now,fail=1 \
+        "$not_existing_in_test_suite_COMMAND"
+  else
+    parallel \
+      --halt-on-error now,fail=1 \
+      "$not_existing_in_test_suite_COMMAND" \
+      ::: \
+      features \
+      spec
+  fi
 
   if [ $? -eq 0 ]; then
     echo $1
