23808b6f8e0a60e4f1bb524ee5142ab4e398c62e
Marcin Nowicki <pr0d1r2@gmail.com>
2017-06-14 10:30:01 +0200

    Improve syntax to be more readable

--- vcr_cassettes_list.sh
+++ vcr_cassettes_list.sh
@@ -1,3 +1,5 @@
 vcr_cassettes_list() {
-  find spec/fixtures/cassettes -name "*.yml" | parallel basename {} | cut -f 1 -d .
+  find spec/fixtures/cassettes -name '*.yml' | \
+    parallel 'basename {}' | \
+    cut -f 1 -d .
 }
