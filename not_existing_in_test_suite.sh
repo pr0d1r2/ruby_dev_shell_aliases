470dbdf7820511f98381522304a4e61a70bde5f5
Marcin Nowicki <pr0d1r2@gmail.com>
2017-06-14 11:37:12 +0200

    Revert further paralellization due to performance degradation

--- not_existing_in_test_suite.sh
+++ not_existing_in_test_suite.sh
@@ -1,27 +1,14 @@
 not_existing_in_test_suite() {
-  local not_existing_in_test_suite_COMMAND
-  not_existing_in_test_suite_COMMAND="rg --quiet $1 {};"
-  not_existing_in_test_suite_COMMAND+='if [ $? -eq 0 ]; then exit 1; else exit 0; fi'
-
   if [ -f .vcr_used_in ]; then
-    cat .vcr_used_in | \
-      parallel \
-        --halt-on-error now,fail=1 \
-        "$not_existing_in_test_suite_COMMAND" \
-        &>/dev/null
+    rg --quiet $1 `cat .vcr_used_in | tr -d "\n"`
   else
-    parallel \
-      --halt-on-error now,fail=1 \
-      "$not_existing_in_test_suite_COMMAND" \
-      ::: \
-      features \
-      spec \
-      &>/dev/null
+    rg --quiet $1 features spec
   fi
 
-  if [ $? -eq 0 ]; then
+  local not_existing_in_test_suite_STATUS=$?
+
+  if [ $not_existing_in_test_suite_STATUS -eq 1 ]; then
     echo $1
-    return 1
   fi
-  return 0
+  return $not_existing_in_test_suite_STATUS
 }
