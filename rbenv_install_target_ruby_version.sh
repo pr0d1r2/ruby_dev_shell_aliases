9b30b023b47abe2169c873f4f1b1b36f41518120
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-03 08:56:19 +0200

    Conform to ShellCheck

--- rbenv_install_target_ruby_version.sh
+++ rbenv_install_target_ruby_version.sh
@@ -1,27 +1,28 @@
 function rbenv_install_target_ruby_version() {
   if [ -f .rubocop.yml ]; then
-    cat .rubocop.yml | grep -q TargetRubyVersion:
-    if [ $? -eq 0 ]; then
-      local rbenv_install_target_ruby_version_VERSION=`echo $(cat .rubocop.yml | grep TargetRubyVersion: | cut -f 2 -d :)`
-      cd ~/.rbenv/plugins/ruby-build || return 1003
-      git pull || git pull || return 1004
-      cd - || return 1005
-      local rbenv_install_target_ruby_version_PATCHED_VERSION=`echo $(rbenv install --list | grep "  $rbenv_install_target_ruby_version_VERSION" | sort | tail -n 1)`
-      rbenv version | cut -f 1 -d ' ' | grep -q $rbenv_install_target_ruby_version_PATCHED_VERSION
-      if [ $? -gt 0 ]; then
-        rbenv local $rbenv_install_target_ruby_version_PATCHED_VERSION
-        if [ $? -gt 0 ]; then
-          rbenv install $rbenv_install_target_ruby_version_PATCHED_VERSION
-          rbenv local $rbenv_install_target_ruby_version_PATCHED_VERSION || return 1006
+    if (grep -q TargetRubyVersion: < .rubocop.yml); then
+      local rbenv_install_target_ruby_version_VERSION
+      rbenv_install_target_ruby_version_VERSION=$(grep TargetRubyVersion: < .rubocop.yml | cut -f 2 -d : | cut -b2-)
+      cd ~/.rbenv/plugins/ruby-build || return 203
+      git pull || git pull || return 204
+      cd - || return 205
+      local rbenv_install_target_ruby_version_PATCHED_VERSION
+      rbenv_install_target_ruby_version_PATCHED_VERSION=$(
+        rbenv install --list | grep "  $rbenv_install_target_ruby_version_VERSION" | sort | tail -n 1 | cut -b3-
+      )
+      if ! (rbenv version | cut -f 1 -d ' ' | grep -q "$rbenv_install_target_ruby_version_PATCHED_VERSION"); then
+        if ! (rbenv local "$rbenv_install_target_ruby_version_PATCHED_VERSION") then
+          rbenv install "$rbenv_install_target_ruby_version_PATCHED_VERSION"
+          rbenv local "$rbenv_install_target_ruby_version_PATCHED_VERSION" || return 206
         fi
       fi
-      bi || return 1007
+      bi || return 207
     else
       echo "No TargetRubyVersion in .rubocop.yml file. Exiting ..."
-      return 1002
+      return 202
     fi
   else
     echo "No .rubocop.yml file. Exiting ..."
-    return 1001
+    return 201
   fi
 }
