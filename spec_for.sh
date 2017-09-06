c345e9f381749c12af8047a748131f2bfc763b90
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-06 06:53:45 +0200

    Support for specs in engines

--- spec_for.sh
+++ spec_for.sh
@@ -1,24 +1,20 @@
 function spec_for() {
   local spec_for_FILE
   local spec_for_SPEC_FILE
-  local spec_for_FILE_IN_DIRECTORY
-  for spec_for_FILE in $@
+  for spec_for_FILE in "$@"
   do
-    if [ -d $spec_for_FILE ]; then
-      for spec_for_FILE_IN_DIRECTORY in `find $spec_for_FILE -type f`
-      do
-        spec_for $spec_for_FILE_IN_DIRECTORY
-      done
+    if [ -d "$spec_for_FILE" ]; then
+      find "$spec_for_FILE" -type f -name "*.rb" | \
+        parallel "source $RUBY_DEV_SHELL_ALIASES_PATH/spec_for.sh && spec_for {}" 2>/dev/null
     else
-      spec_for_SPEC_FILE=`echo $spec_for_FILE | sed -e "s/app\//spec\//" | sed -e 's/.rb$/_spec.rb/' | sed -e 's/.slim$/.slim_spec.rb/'`
+      spec_for_SPEC_FILE="$(echo "$spec_for_FILE" | sed -e "s/app\//spec\//" -e 's/.rb$/_spec.rb/' -e 's/.slim$/.slim_spec.rb/')"
       case $spec_for_SPEC_FILE in
-        spec/*)
-          ;;
+        spec/* | engines/*) ;;
         *)
           spec_for_SPEC_FILE="spec/$spec_for_SPEC_FILE"
           ;;
       esac
-      echo $spec_for_SPEC_FILE | grep --color=never "_spec.rb$" | sed -e 's/_spec_spec.rb$/_spec.rb/'
+      echo "$spec_for_SPEC_FILE" | grep --color=never "_spec.rb$" | sed -e 's/_spec_spec.rb$/_spec.rb/'
     fi
   done
 }
