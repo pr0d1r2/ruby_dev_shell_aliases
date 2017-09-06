5474ebe04f5bb0d1bc1b4f3eede8a13e318f9651
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-06 05:32:27 +0200

    Improve guard stop script

--- guard_stop_all.sh
+++ guard_stop_all.sh
@@ -1,7 +1,5 @@
 function guard_stop_all() {
   kill_forcefully \
-    "$(ps -ax | pgrep guard-rspec | cut -b1-5)" \
-    "$(ps -ax | pgrep _guard-core | cut -b1-5)" \
-    "$(ps -ax | pgrep bin/guard | cut -b1-5)"
+    "$(parallel "pgrep -f {}" ::: "gems/guard-rspec" "ruby .*bin/_guard-core" "bin/guard" 2>/dev/null)"
   return $?
 }
