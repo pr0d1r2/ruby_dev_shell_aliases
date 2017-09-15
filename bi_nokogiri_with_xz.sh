b09e5a58f117fa80c84f901122c468fc74e9f3b7
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-15 07:24:51 +0200

    Silence bundle install

--- bi_nokogiri_with_xz.sh
+++ bi_nokogiri_with_xz.sh
@@ -4,7 +4,7 @@ function bi_nokogiri_with_xz() {
   if [ $bi_nokogiri_with_xz_NOT_PRESENT -eq 0 ]; then
     brew uninstall --ignore-dependencies --force xz || return $?
   fi
-  bi || return $?
+  bi_quiet || return $?
   if [ $bi_nokogiri_with_xz_NOT_PRESENT -eq 0 ]; then
     brew install xz || return $?
   fi
