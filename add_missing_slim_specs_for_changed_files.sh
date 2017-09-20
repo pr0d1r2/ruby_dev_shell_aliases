826eb1bfd87a3962e5e5092b775c931dbf698da3
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-20 04:33:03 +0200

    Use shorter syntax

--- add_missing_slim_specs_for_changed_files.sh
+++ add_missing_slim_specs_for_changed_files.sh
@@ -2,5 +2,5 @@ function add_missing_slim_specs_for_changed_files() {
   git st  | grep slim | cut -b4- | \
     parallel "source ~/projects/ruby_dev_shell_aliases/spec_for.sh && spec_for {}" | \
       parallel "test -f {} || (test -d {//} || mkdir -p {//} ; touch {} && git add {} && echo {})" | \
-        parallel 'echo "RSpec.describe \"$(echo {} | sed -e "s|^spec/views/||g" -e "s/\.html\.slim_spec\.rb$//g")\" do\n  specify do\n    expect(subject).to have_text(\"TODO\")\n  end\nend" > {}'
+        parallel 'echo "RSpec.describe \"$(echo {} | sed -e "s|^spec/views/||g" -e "s/\.html\.slim_spec\.rb$//g")\" do\n  specify do\n    is_expected.to have_text(\"TODO\")\n  end\nend" > {}'
 }
