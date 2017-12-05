ecf60d1abb9e01798ebb63bc453d222709ea0524
Marcin Nowicki <pr0d1r2@gmail.com>
2017-12-05 05:42:28 +0100

    Add docs

--- spec_directories.sh
+++ spec_directories.sh
@@ -1,3 +1,7 @@
+# List existing spec directories in the project
+#
+# Example usage:
+#   spec_directories
 function spec_directories() {
   ls -d spec engines/*/spec gems/*/spec 2>/dev/null || return $?
 }
