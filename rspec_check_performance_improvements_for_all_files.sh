5aee53af880f656f55e90c1377d415d521beab7f
Marcin Nowicki <marcin.nowicki@toptal.com>
2017-12-14 10:31:05 +0200

    Fix performance checking

--- rspec_check_performance_improvements_for_all_files.sh
+++ rspec_check_performance_improvements_for_all_files.sh
@@ -1,16 +1,16 @@
 function rspec_check_performance_improvements_for_all_files() {
   local rspec_check_performance_improvements_for_all_files_SPECS
-  rspec_check_performance_improvements_for_all_files_SPECS=$(
+  rspec_check_performance_improvements_for_all_files_SPECS=($(
     git status -sb | grep -E "^( M |M  |MM )" | grep "_spec\.rb$" | cut -b4-
-  )
+  ))
   echo
   echo 'Before'
   git stash
-  bin/rspec -f p "$rspec_check_performance_improvements_for_all_files_SPECS" || return $?
+  echorun bundle exec rspec -f p $rspec_check_performance_improvements_for_all_files_SPECS || return $?
   echo
   echo 'After'
   git stash pop
-  bin/rspec -f p "$rspec_check_performance_improvements_for_all_files_SPECS" || return $?
+  echorun bundle exec rspec -f p $rspec_check_performance_improvements_for_all_files_SPECS || return $?
   echo
   echo
 }
