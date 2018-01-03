fe10ff9257572b40cafde4cb22440863ccea5dbf
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-03 13:31:34 +0100

    Simplify implementation

--- rspec_files_containing_shared_examples_from_file.sh
+++ rspec_files_containing_shared_examples_from_file.sh
@@ -7,15 +7,10 @@ function rspec_files_containing_shared_examples_from_file() {
     "grep shared_examples {} | \
      cut -f 2 -d \"'\" | \
      parallel -I '<>' \"
-       source $HOME/projects/ruby_dev_shell_aliases/spec_directories.sh && \
-       spec_directories | \
-         parallel -I '[]' -j 1 \
-           \\\"
-           rg <> [] | \
-           grep -E 'include_examples|it_behaves_like|it_should_behave_like' | \
-           cut -f 1 -d : | \
-           sort -u
-           \\\"
+        rg <> $(spec_directories | tr "\n" ' ') | \
+        grep -E 'include_examples|it_behaves_like|it_should_behave_like' | \
+        cut -f 1 -d : | \
+        sort -u
      \" | \
      grep \"\\\.rb$\" | \
      sort -u
@@ -27,6 +22,7 @@ function rspec_files_containing_shared_examples_from_file() {
            echo {}
          else
            echo FORK {}
+           source $HOME/projects/ruby_dev_shell_aliases/spec_directories.sh && \
            source $HOME/projects/ruby_dev_shell_aliases/rspec_files_containing_shared_examples_from_file.sh && \
            rspec_files_containing_shared_examples_from_file {}
            echo POST FORK
