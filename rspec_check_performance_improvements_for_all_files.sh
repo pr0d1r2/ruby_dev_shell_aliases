eb20747db4e393ad33034c13bca037190e74832f
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-04 14:43:39 +0100

    Improve syntax

--- rspec_check_performance_improvements_for_all_files.sh
+++ rspec_check_performance_improvements_for_all_files.sh
@@ -2,7 +2,10 @@ function rspec_check_performance_improvements_for_all_files() {
   git_not_clean || return $?
   local rspec_check_performance_improvements_for_all_files_SPECS
   rspec_check_performance_improvements_for_all_files_SPECS=($(
-    git status -sb | grep -E "^( M |M  |MM )" | grep "_spec\.rb$" | cut -b4-
+    git status -sb | \
+      grep -E "^( M |M  |MM )" | \
+      grep "_spec\.rb$" | \
+      cut -b4-
   ))
   echo
   echo 'Before'
