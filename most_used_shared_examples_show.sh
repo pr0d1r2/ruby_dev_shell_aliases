45fe90f11c40c5995f2b19c7e151aac7d18cc471
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-05 06:39:11 +0100

    Simplify implementation. Fix indentation

--- most_used_shared_examples_show.sh
+++ most_used_shared_examples_show.sh
@@ -1,6 +1,6 @@
 function most_used_shared_examples_show() {
   test -d tmp/most_used_shared_examples || echorun most_used_shared_examples_determine || return $?
   find tmp/most_used_shared_examples -size +0 -type f -exec ls -lSr {} + | \
-      awk '{ print $9 }' | parallel "wc -l {}" | \
-        parallel "echo {} | sed -e 's|tmp/most_used_shared_examples/||'" -e 's/.list$//' | sort
+    awk '{ print $9 }' | \
+    parallel "wc -l {} | sed -e 's|tmp/most_used_shared_examples/||'" -e 's/.list$//' | sort
 }
