a02f9664859553a7466c10530dc0447facec638c
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-05 06:35:25 +0100

    Fix checking previous generation of file

--- most_used_shared_examples_determine.sh
+++ most_used_shared_examples_determine.sh
@@ -2,7 +2,7 @@ function most_used_shared_examples_determine() {
   find . -name shared_examples -type d | \
     parallel "find {} -type f -name '*.rb'" | \
       parallel -j1 \
-        "test -f most_used_shared_examples-{/}.list && exit 0
+        "test -f most_used_shared_examples/{}.list && exit 0
          test -d most_used_shared_examples/{//} || mkdir -p most_used_shared_examples/{//} && \
          source $HOME/.compiled_shell_aliases.sh && \
          rspec_files_containing_shared_examples_from_file {} > most_used_shared_examples/{}.list" || return $?
