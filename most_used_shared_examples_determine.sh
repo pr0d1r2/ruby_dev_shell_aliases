686d3868e531cf7551a7c5a47f47457aabd26f1c
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-05 06:37:46 +0100

    Store most_used_shared_examples in tmp

--- most_used_shared_examples_determine.sh
+++ most_used_shared_examples_determine.sh
@@ -2,8 +2,8 @@ function most_used_shared_examples_determine() {
   find . -name shared_examples -type d | \
     parallel "find {} -type f -name '*.rb'" | \
       parallel -j1 \
-        "test -f most_used_shared_examples/{}.list && exit 0
-         test -d most_used_shared_examples/{//} || mkdir -p most_used_shared_examples/{//} && \
+        "test -f tmp/most_used_shared_examples/{}.list && exit 0
+         test -d tmp/most_used_shared_examples/{//} || mkdir -p tmp/most_used_shared_examples/{//} && \
          source $HOME/.compiled_shell_aliases.sh && \
-         rspec_files_containing_shared_examples_from_file {} > most_used_shared_examples/{}.list" || return $?
+         rspec_files_containing_shared_examples_from_file {} > tmp/most_used_shared_examples/{}.list" || return $?
 }
