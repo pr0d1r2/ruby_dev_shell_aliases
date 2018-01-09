45d2796044b18995e4dcacf8991a462f754f4dd3
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-09 07:40:42 +0100

    Improve flakiness script to check all random seeds

--- rspec_check_for_flakiness.sh
+++ rspec_check_for_flakiness.sh
@@ -1,4 +1,5 @@
-# Check for flakiness in RSpec example or file (in parallel)
+# Check for flakiness in RSpec example or spec file.
+# It checks example or spec file with all possible random seeds in parallel.
 #
 # Example usage:
 #   rspec_check_for_flakiness spec/my_spec.rb
@@ -6,8 +7,7 @@
 function rspec_check_for_flakiness() {
   rspec_check_for_flakiness_COMMAND="source $HOME/projects/common_shell_aliases/silently.sh && "
   rspec_check_for_flakiness_COMMAND+="source $HOME/projects/osx_shell_aliases/md5sum.sh && " ##Darwin
-  rspec_check_for_flakiness_COMMAND+="source $HOME/projects/common_shell_aliases/until_failure.sh && "
-  rspec_check_for_flakiness_COMMAND+="SLEEP=1 ATTEMPTS=20 TEST_ENV_NUMBER={} until_failure silently bundle exec rspec -f p $*"
-  cpu_list | parallel --halt now,fail=1 "$rspec_check_for_flakiness_COMMAND"
+  rspec_check_for_flakiness_COMMAND+="TEST_ENV_NUMBER={%} silently bundle exec rspec --seed {} -f p $*"
+  seq 65535 | parallel --halt now,fail=1 "$rspec_check_for_flakiness_COMMAND"
   return $?
 }
