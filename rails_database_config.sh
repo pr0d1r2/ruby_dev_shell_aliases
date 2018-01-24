935d5a1500a0a3362bcb82f6e67638caba2e7c00
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-24 18:15:07 +0100

    Add caching for faster operations

--- rails_database_config.sh
+++ rails_database_config.sh
@@ -14,8 +14,17 @@ function rails_database_config() {
     "") rails_database_config_WORKING_DIR=".";;
     *) rails_database_config_WORKING_DIR="$3";;
   esac
-  ruby \
-    -X "$rails_database_config_WORKING_DIR" \
-    -e "$(rails_database_config_snippet)['$rails_database_config_ENV']['$rails_database_config_NAME']"
+  local rails_database_config_COMMAND
+  local rails_database_config_CACHE_KEY
+  local rails_database_config_CACHE_FILE
+  rails_database_config_COMMAND="$(rails_database_config_snippet)['$rails_database_config_ENV']['$rails_database_config_NAME']"
+  rails_database_config_CACHE_KEY="$(cd "$rails_database_config_WORKING_DIR" && pwd -P)___$rails_database_config_COMMAND"
+  rails_database_config_CACHE_FILE="/tmp/.cache-rails_database_config-$(echo "$rails_database_config_CACHE_KEY" | shasum | cut -f 1 -d ' ')"
+  if [ ! -f "$rails_database_config_CACHE_FILE" ]; then
+    ruby \
+      -X "$rails_database_config_WORKING_DIR" \
+      -e "$rails_database_config_COMMAND" > "$rails_database_config_CACHE_FILE" || return $?
+  fi
+  cat "$rails_database_config_CACHE_FILE" || return $?
   return $?
 }
