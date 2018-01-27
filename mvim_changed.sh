e0ca23ddb81fdfe7ec5769fb9da4a6b7b4f4f9e8
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-27 07:49:49 +0100

    Use GNU parallel

--- mvim_changed.sh
+++ mvim_changed.sh
@@ -1,9 +1,4 @@
 function mvim_changed() {
-  local mvim_changed_FILE
-  for mvim_changed_FILE in `git status --porcelain | grep -v "^D" | cut -c4-`
-  do
-    echo mvim $mvim_changed_FILE
-    mvim $mvim_changed_FILE &
-  done
-  wait
+  git status --porcelain | grep -v "^D" | cut -c4- | \
+    parallel "mvim {}"
 }
