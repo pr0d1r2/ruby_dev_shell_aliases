7155f5335fa9ea56bd365428782dd38222e4bcbb
Marcin Nowicki <pr0d1r2@gmail.com>
2018-04-05 06:11:46 +0200

    Fix libxml2 detection

--- use_homebrew_libxml2_on_macos.sh
+++ use_homebrew_libxml2_on_macos.sh
@@ -8,8 +8,9 @@ function use_homebrew_libxml2_on_macos() {
   esac
   local use_homebrew_libxml2_on_macos_CONFIG
   use_homebrew_libxml2_on_macos_CONFIG="$(ls /usr/local/Cellar/libxml2/*/bin/xml2-config)"
-  if [ -e "$use_homebrew_libxml2_on_macos_CONFIG" ]; then
+  if [ $? -gt 0 ]; then
     echorun brew install libxml2 || return $?
+    use_homebrew_libxml2_on_macos_CONFIG="$(ls /usr/local/Cellar/libxml2/*/bin/xml2-config)"
   fi
   echorun bundle config build.libxml-ruby --with-xml2-config="$use_homebrew_libxml2_on_macos_CONFIG"
 }
