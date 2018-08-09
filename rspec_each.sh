009291d307b958baca92e464ca363c3686844d28
Marcin Nowicki <pr0d1r2@gmail.com>
2018-08-09 09:52:53 +0200

    Improve rspec each to have timeout

--- rspec_each.sh
+++ rspec_each.sh
@@ -2,19 +2,23 @@
 #
 # Useful when you want to test all files separately
 # or in case of performance testing on another core[s].
+# Assume 19s timeout (assuming 16s wait time and 3s loading time).
 #
 # Example usage (all files changed):
 #   rspec_each
 function rspec_each() {
+  echorun spring_ensure_started rspec || return $?
   local rspec_each_COMMAND
   rspec_each_COMMAND="source $HOME/projects/common_shell_aliases/silently.sh && "
   rspec_each_COMMAND+="source $HOME/projects/osx_shell_aliases/md5sum.sh && " ##Darwin
-  rspec_each_COMMAND+="silently rspec -f p $* {}"
+  rspec_each_COMMAND+="silently gtimeout --foreground 19 spring rspec -f p $* {}"
   case $1 in
     "")
+      changed_specs | parallel -v -j 1 "$rspec_each_COMMAND"
+      ;;
+    *)
+      parallel -v -j 1 "$rspec_each_COMMAND" ::: $*
       ;;
   esac
-  changed_specs | parallel -v -j 1 "$rspec_each_COMMAND"
-
   return $?
 }
---
+++ spring_ensure_started.sh
@@ -0,0 +1,9 @@
+# Ensure spring is started.
+# Useful when runing commands on GNU parallel or xargs
+#
+# Example usage:
+#   spring_ensure_started && parallel "spring rake {}" ::: foo bar baz
+function spring_ensure_started() {
+  spring status | grep -q "is running" || \
+    spring "$@" --help &>/dev/null || return $?
+}
