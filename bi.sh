51ab78c6e2b7aea5bbd57f440ce8c502217729af
Marcin Nowicki <pr0d1r2@gmail.com>
2018-09-12 07:40:02 +0200

    Conform to ShellCheck

--- bi.sh
+++ bi.sh
@@ -1,23 +1,23 @@
 unalias bi 2>/dev/null
 function bi() {
   if [ -e Gemfile.lock ]; then
-    cat Gemfile.lock | grep -q 'BUNDLED WITH'
-    if [ $? -eq 0 ]; then
-      local bi_BUNDLED_WITH_VERSION=`echo $(tail -n1 Gemfile.lock)`
-      local bi_BUNDLER_VERSION_PARAM="_${bi_BUNDLED_WITH_VERSION}_"
-      case `bundle $bi_BUNDLER_VERSION_PARAM version | cut -f 3 -d ' '` in
-        $bi_BUNDLED_WITH_VERSION)
+    if grep -q 'BUNDLED WITH' Gemfile.lock; then
+      local bi_BUNDLED_WITH_VERSION
+      local bi_BUNDLER_VERSION_PARAM
+      bi_BUNDLED_WITH_VERSION=$(tail -n1 Gemfile.lock | awk '{gsub(/^ +| +$/,"")}1')
+      bi_BUNDLER_VERSION_PARAM="_${bi_BUNDLED_WITH_VERSION}_"
+      case "$(bundle "$bi_BUNDLER_VERSION_PARAM" version | cut -f 3 -d ' ')" in
+        "$bi_BUNDLED_WITH_VERSION")
           ;;
         *)
-          echorun gem install bundler -v $bi_BUNDLED_WITH_VERSION
+          echorun gem install bundler -v "$bi_BUNDLED_WITH_VERSION"
           ;;
       esac
     fi
   fi
-  is_new_bundler
-  if [ $? -eq 0 ]; then
-    echorun bundle $bi_BUNDLER_VERSION_PARAM install -j`bundler_threads` || return $?
+  if is_new_bundler; then
+    echorun bundle "$bi_BUNDLER_VERSION_PARAM" install -j "$(bundler_threads)" || return $?
   else
-    echorun bundle $bi_BUNDLER_VERSION_PARAM install || return $?
+    echorun bundle "$bi_BUNDLER_VERSION_PARAM" install || return $?
   fi
 }
