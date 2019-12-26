79aa6c835d4dc760dc062d63bc57820095ee996a
Marcin Nowicki <pr0d1r2@gmail.com>
2019-12-26 10:46:11 +0100

    Install required ruby version before bundling

--- bi.sh
+++ bi.sh
@@ -1,5 +1,23 @@
-unalias bi 2>/dev/null
+# Installs full ruby project bundle with required ruby and bundler versions
+#
+# Example usage:
+#   bi
+unalias bi 2>/dev/null # unaliasing
 function bi() {
+  if [ -e .ruby-version ]; then
+    if [ -x "$HOME/.rbenv/bin/rbenv" ]; then
+      local bi_RUBY_VERSION
+      bi_RUBY_VERSION="$(cat .ruby-version | sed -e 's/ruby-//g')"
+      if ($HOME/.rbenv/bin/rbenv local "$bi_RUBY_VERSION" 2>&1 | grep -q "not installed"); then
+        echo "Ruby version $bi_RUBY_VERSION not present, installing..."
+        cd "$HOME/.rbenv/plugins/ruby-build" || return $?
+        git pull || return $?
+        cd - || return $?
+        $HOME/.rbenv/bin/rbenv install "$bi_RUBY_VERSION" || return $?
+        $HOME/.rbenv/bin/rbenv local "$bi_RUBY_VERSION" || return $?
+      fi
+    fi
+  fi
   if [ -e Gemfile.lock ]; then
     if grep -q 'BUNDLED WITH' Gemfile.lock; then
       local bi_BUNDLED_WITH_VERSION
