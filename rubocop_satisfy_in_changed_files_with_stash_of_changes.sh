56e0aa862e791ecb8668449abdab8560373b61e3
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-11 10:11:41 +0200

    Simplify syntax

--- rubocop_satisfy_in_changed_files_with_stash_of_changes.sh
+++ rubocop_satisfy_in_changed_files_with_stash_of_changes.sh
@@ -6,10 +6,7 @@
 # Or if you want to run extra command in the chain:
 #   rubocop_satisfy_in_changed_files_with_stash_of_changes my_command with parameters
 function rubocop_satisfy_in_changed_files_with_stash_of_changes() {
-  local rubocop_satisfy_in_changed_files_with_stash_of_changes_LIST
   local rubocop_satisfy_in_changed_files_with_stash_of_changes_COMMAND
-  rubocop_satisfy_in_changed_files_with_stash_of_changes_LIST=()
-  mapfile -t rubocop_satisfy_in_changed_files_with_stash_of_changes_LIST < <(git status -sb | cut -b4-)
   rubocop_satisfy_in_changed_files_with_stash_of_changes_COMMAND="source $HOME/projects/common_shell_aliases/silently.sh && "
   rubocop_satisfy_in_changed_files_with_stash_of_changes_COMMAND+="source $HOME/projects/osx_shell_aliases/md5sum.sh && " ##Darwin
   rubocop_satisfy_in_changed_files_with_stash_of_changes_COMMAND+="silently rubocop -a {}"
@@ -21,9 +18,8 @@ function rubocop_satisfy_in_changed_files_with_stash_of_changes() {
   esac
 
   OVERCOMMIT_DISABLE=1 git stash || return $?
+  git status -sb | cut -b4- | \
   parallel \
-    "$rubocop_satisfy_in_changed_files_with_stash_of_changes_COMMAND" \
-    ::: \
-    "${rubocop_satisfy_in_changed_files_with_stash_of_changes_LIST[@]}"
+    "$rubocop_satisfy_in_changed_files_with_stash_of_changes_COMMAND"
   return $?
 }
