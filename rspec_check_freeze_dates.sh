56f06fe3d9fed4e06048f19d9f271ca6c764e626
Marcin Nowicki <pr0d1r2@gmail.com>
2020-07-16 10:38:09 +0200

    Only show successful cases

--- rspec_check_freeze_dates.sh
+++ rspec_check_freeze_dates.sh
@@ -1,4 +1,5 @@
 # Check freezing time in examples for file changes timestamps
+# It outputs only successful cases
 #
 # Example usage:
 #   rspec_check_freeze_dates spec/my_spec.rb
@@ -8,9 +9,13 @@
 #   FREEZE=2020-06-15 TEST_ENV_NUMBER=3 bundle exec rspec spec/my_spec.rb
 #   ...
 #   FREEZE=YYYY-MM-DD TEST_ENV_NUMBER=N bundle exec rspec spec/my_spec.rb
-
 function rspec_check_freeze_dates() {
-  git_file_changed_dates $1 | \
-    parallel "FREEZE={} TEST_ENV_NUMBER={%} bundle exec rspec $1"
+  local rspec_check_freeze_dates_COMMAND
+  rspec_check_freeze_dates_COMMAND="source $HOME/projects/common_shell_aliases/silently.sh"
+  rspec_check_freeze_dates_COMMAND+="; source $HOME/projects/common_shell_aliases/silently_when_failing.sh"
+  rspec_check_freeze_dates_COMMAND+="; source $HOME/projects/osx_shell_aliases/md5sum.sh" ## Darwin
+  # shellcheck disable=SC2124
+  rspec_check_freeze_dates_COMMAND+="; FREEZE={} TEST_ENV_NUMBER={%} silently_when_failing bundle exec rspec $1 && echo 'OK: {}'"
+  git_file_changed_dates $1 | parallel "$rspec_check_freeze_dates_COMMAND"
   return $?
 }
