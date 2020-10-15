a7ad46eff829de93c240191e62905a4fed4b2acf
Marcin Nowicki <pr0d1r2@gmail.com>
2020-10-15 06:33:46 +0200

    Allow to set postgres version

--- postgres_ensure.sh
+++ postgres_ensure.sh
@@ -1,7 +1,7 @@
 function postgres_ensure() {
   if (command -v postgres &>/dev/null); then
-    echorun brew install postgresql || return $?
-    echorun brew services start postgresql || return $?
+    echorun brew install postgresql$1 || return $?
+    echorun brew services start postgresql$1 || return $?
   fi
   return 0
 }
