1656af26d4d2918d38b4211f74ce12b26f02f1d5
Marcin Nowicki <pr0d1r2@gmail.com>
2020-11-24 13:13:52 +0100

    Remove temporary file

--- export_bundle_config_variables.sh
+++ export_bundle_config_variables.sh
@@ -4,4 +4,5 @@ function export_bundle_config_variables() {
   cat .bundle/config | grep ": " | sed -e "s/: /=/" > "$export_bundle_config_variables_TMP" || return $?
   cat .bundle/config | grep ": " | cut -f 1 -d : | parallel -j 1 "echo export {} >> '$export_bundle_config_variables_TMP'" || return $?
   source "$export_bundle_config_variables_TMP"
+  rm -f "$export_bundle_config_variables_TMP"
 }
