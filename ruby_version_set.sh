c8319901f1afe588dc58216ed60453fe74a45fdd
Marcin Nowicki <pr0d1r2@gmail.com>
2020-12-02 19:45:24 +0100

    Allow quickly change ruby version

---
+++ ruby_version_set.sh
@@ -0,0 +1,14 @@
+function ruby_version_set() {
+  local ruby_version_set_VERSION
+  case $1 in
+    [0-9].[0-9] | [0-9].[0-9].[0-9])
+      ruby_version_set_VERSION="$1"
+      ;;
+    *)
+      ruby_version_set_VERSION="2.7.2"
+      ;;
+  esac
+  echo "$ruby_version_set_VERSION" > .ruby-version || return $?
+  sed -ie "s/^ruby '[0-9.]\{1,\}'/ruby '$ruby_version_set_VERSION'/" Gemfile || return $?
+  bi || return $?
+}
