2d9c846fbfa567dc977cd2a71bb11148d2912ec0
Marcin Nowicki <pr0d1r2@gmail.com>
2021-05-20 15:52:57 +0200

    Add script useful for reruning branch on guard

---
+++ poke_files_changed_in_branch.sh
@@ -0,0 +1,6 @@
+function poke_files_changed_in_branch() {
+  local poke_files_changed_in_branch_ID
+  poke_files_changed_in_branch_ID="# POKE_FILES_CHANGED_IN_BRANCH_$(datetimeh)"
+  git_files_changed_in_branch | \
+    parallel "echo -n '$poke_files_changed_in_branch_ID' >> {} && grep -v '$poke_files_changed_in_branch_ID' {} > {}.tmp && mv {}.tmp {}"
+}
