24cd24a06cc9bf8d874a51659688667860778146
Marcin Nowicki <pr0d1r2@gmail.com>
2021-08-20 11:12:41 +0200

    Do not poke Guardfile as we are doing this to trigger guard on changeset files

--- poke_files_changed_in_branch.sh
+++ poke_files_changed_in_branch.sh
@@ -1,6 +1,6 @@
 function poke_files_changed_in_branch() {
   local poke_files_changed_in_branch_ID
   poke_files_changed_in_branch_ID="# POKE_FILES_CHANGED_IN_BRANCH_$(datetimeh)"
-  git_files_changed_in_branch | \
+  git_files_changed_in_branch | grep -v "^Guardfile$" | \
     parallel "echo -n '$poke_files_changed_in_branch_ID' >> {} && grep -v '$poke_files_changed_in_branch_ID' {} > {}.tmp && mv {}.tmp {}"
 }
