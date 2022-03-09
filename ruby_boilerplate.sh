54c77d46adf5aa48e08534b202626e6467027e11
Marcin Nowicki <pr0d1r2@gmail.com>
2022-03-09 11:37:19 +0100

    Add quick boilerplate generator for ruby projects

---
+++ ruby_boilerplate.sh
@@ -0,0 +1,36 @@
+function ruby_boilerplate() {
+  test -f .ruby-version || echo 3.0.0 > .ruby-version
+  test -f Gemfile || echo "# frozen_string_literal: true
+
+ruby '3.0.0'
+
+group :development do
+  gem 'rspec', require: false
+  gem 'guard-rspec', require: false
+  gem 'guard-rubocop', require: false
+  gem 'rubocop-rspec', require: false
+end" > Gemfile
+
+  bundle install || return $?
+
+  if [ ! -f Guardfile ]; then
+    bundle exec guard init rspec || return $?
+    bundle exec guard init rubocop || return $?
+  fi
+
+  if [ ! -f .rubocop_todo.yml ]; then
+    bundle exec rubocop --auto-gen-config
+  fi
+
+  if ! (grep rubocop-rspec .rubocop.yml); then
+    echo "require: rubocop-rspec" >> .rubocop.yml
+    bundle exec rubocop --auto-gen-config
+  fi
+
+  if ! (grep 'NewCops: enable' .rubocop.yml); then
+    echo "AllCops:
+  NewCops: enable
+  TargetRubyVersion: 3.0.0" >> .rubocop.yml
+    bundle exec rubocop --auto-gen-config
+  fi
+}
