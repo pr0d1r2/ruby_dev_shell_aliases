c1f14862b7b01a44a62cdf0eaa9ed561282fe83c
Marcin Nowicki <pr0d1r2@gmail.com>
2022-11-19 07:55:22 +0100

    Skip deleted files

--- rubocop_autofix_removed_todo.sh
+++ rubocop_autofix_removed_todo.sh
@@ -2,7 +2,7 @@ function rubocop_autofix_removed_todo() {
   local rubocop_autofix_removed_todo_FILES
   local rubocop_autofix_removed_todo_RUBOCOP_FILES
   rubocop_autofix_removed_todo_FILES=$()
-  rubocop_autofix_removed_todo_RUBOCOP_FILES=$(be rubocop --parallel $(rubocop_todo_removed_files) | grep "rb:" | cut -f 1 -d : | sort -u)
+  rubocop_autofix_removed_todo_RUBOCOP_FILES=$(be rubocop --parallel $(rubocop_todo_removed_files | parallel "test -f {} && echo {}") | grep "rb:" | cut -f 1 -d : | sort -u)
   if [ $(echo $rubocop_autofix_removed_todo_RUBOCOP_FILES | wc -l) -gt 1 ]; then
     bundle exec rubocop -A $(echo $rubocop_autofix_removed_todo_RUBOCOP_FILES)
   fi
