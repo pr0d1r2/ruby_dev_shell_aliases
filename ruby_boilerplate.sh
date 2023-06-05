a69766f0c0dc052192783bfed7263a8de37b32cb
Marcin Nowicki <pr0d1r2@gmail.com>
2023-06-05 09:17:10 +0200

    Add implementation and spec files in green state

--- ruby_boilerplate.sh
+++ ruby_boilerplate.sh
@@ -33,4 +33,31 @@ end" > Gemfile
   TargetRubyVersion: 3.2.2" >> .rubocop.yml
     bundle exec rubocop --auto-gen-config --auto-gen-only-exclude --exclude-limit 10000
   fi
+
+  test -d lib || mkdir lib || return $?
+  test -d spec/lib || mkdir -p spec/lib || return $?
+
+  echo "# frozen_string_literal: true
+
+# Main TDD boilerplate
+class Boilerplate
+  def run
+    'OK'
+  end
+end" > lib/boilerplate.rb
+
+  echo "# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../../lib/boilerplate'
+
+RSpec.describe Boilerplate do
+  subject(:instance) { described_class.new }
+
+  it do
+    expect(instance.run).to eq('OK')
+  end
+end" > spec/lib/boilerplate_spec.rb
+
+  bundle exec rubocop --auto-gen-config --auto-gen-only-exclude --exclude-limit 10000
 }
